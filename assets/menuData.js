5861f3b1df58ec9e47bc769f894fe0ca645ea444
BeyzaKomish <beyzakomis@gmail.com>
2026-01-28 20:29:40 +0300

    a small data array for the menu Items

---
+++ assets/menuData.js
@@ -0,0 +1,54 @@
+export const menuData = [
+  {
+    id: 1,
+    name: "Greek Salad",
+    description:
+      "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.",
+    price: "$12.99",
+    image: "greekSalad",
+    category: "Starters",
+  },
+  {
+    id: 2,
+    name: "Bruschetta",
+    description:
+      "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+    price: "$7.99",
+    image: "bruchetta",
+    category: "Starters",
+  },
+  {
+    id: 3,
+    name: "Grilled Fish",
+    description:
+      "Fish marinated in olive oil, lemon juice, and herbs, grilled to perfection and served with seasonal vegetables.",
+    price: "$18.99",
+    image: "grilledFish",
+    category: "Mains",
+  },
+  {
+    id: 4,
+    name: "Pasta",
+    description:
+      "Penne with fried aubergines, cherry tomatoes, tomato sauce, fresh chilli, garlic, basil and salted ricotta cheese.",
+    price: "$15.99",
+    image: "pasta",
+    category: "Mains",
+  },
+  {
+    id: 5,
+    name: "Lemon Dessert",
+    description:
+      "Traditional homemade Italian Lemon Ricotta Cake, served with a cup of coffee or tea.",
+    price: "$8.99",
+    image: "lemonDessert",
+    category: "Desserts",
+  },
+];
+export const itemImages = {
+  greekSalad: require("./images/Greek-Salad.png"),
+  bruchetta: require("./images/Bruchetta.png"),
+  grilledFish: require("./images/Grilled-Fish.png"),
+  pasta: require("./images/Pasta.png"),
+  lemonDessert: require("./images/Lemon-Dessert.png"),
+};
